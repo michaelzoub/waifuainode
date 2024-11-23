adfb8b54c89c8ee3c0c27e1d1fda04750913525b
michaelzoub <micacao15@gmail.com>
2024-11-23 16:13:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
