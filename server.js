4a20df24ac9390deef53cca12698e546acbe3c62
michaelzoub <micacao15@gmail.com>
2024-11-23 16:55:58 -0500

    update

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -7,8 +10,35 @@ const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 
+ const waifuNames = [
+  "Akiyama Rika", "Takahashi Yumi", "Fujimoto Haruka", "Hoshino Mei", "Sakamoto Rina",
+  "Matsuda Akari", "Tanaka Nao", "Yamashita Sakura", "Hirano Ami", "Suzuki Eri",
+  "Ogawa Yuna", "Ito Nana", "Kobayashi Sora", "Shimizu Rei", "Yamaguchi Kaho",
+  "Kaneko Aoi", "Nishida Miku", "Moriyama Chiyo", "Kondo Sayaka", "Okada Hina",
+  "Hashimoto Mayu", "Kurata Kaede", "Nakamura Mio", "Shiraishi Sana", "Hamada Nozomi",
+  "Ishikawa Riko", "Oda Arisa", "Kawai Chiho", "Matsui Hinata", "Tsubaki Yume",
+  "Saeki Koharu", "Kitagawa Asuka", "Takeda Rin", "Uchida Hana", "Nakagawa Emi",
+  "Saito Rumi", "Tsuchiya Eriko", "Murakami Kira", "Kawakami Yui", "Fujisawa Rio",
+  "Yoshikawa Yuki", "Oshima Ayaka", "Shibata Saya", "Aoyama Yuri", "Takahata Fumi",
+  "Kikuchi Ami", "Nagase Yuka", "Hoshikawa Tomoka", "Tsukamoto Anzu", "Endo Mio"
+];
+
+ const neonColors = [
+  "#FF6EC7", // Neon Pink
+  "#39FF14", // Neon Green
+  "#FFDF00", // Neon Yellow
+  "#FF073A", // Neon Red
+  "#00FFFF", // Neon Cyan
+  "#FF9933", // Neon Orange
+  "#9400D3", // Neon Purple
+  "#7DF9FF", // Neon Sky Blue
+  "#FBEC5D", // Neon Gold
+  "#FE59C2"  // Neon Magenta
+];
+
+
 const corsOptions = {
-  origin: ['https://waifuai-olive.vercel.app', "localhost:3000"],  // Replace with your Vercel app URL
+  origin: ['https://waifuai-olive.vercel.app', "http://localhost:3000"],  // Replace with your Vercel app URL
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
   credentials: true, // Allow cookies if needed
@@ -19,7 +49,7 @@ app.use(cors(corsOptions));
 // Configure CORS to allow requests from localhost:3000
 const io = new Server(server, {
   cors: {
-    origin: ["https://waifuai-olive.vercel.app", "localhost:3000"],  // Allow only requests from this origin
+    origin: ["https://waifuai-olive.vercel.app", "http://localhost:3000"],  // Allow only requests from this origin
     methods: ["GET", "POST"],        // Specify allowed HTTP methods
     allowedHeaders: ["Content-Type"], // Allowed headers
   }
@@ -39,10 +69,22 @@ mongoose.connect(mongoURI, {
     console.error('Error connecting to MongoDB:', err);
   });
 
+//random number between 0-50
+const waifuNumber = Math.floor(Math.random() * 50)
+const colorNumber = Math.floor(Math.random() * 10)
+
 // Define the Message schema
 const messageSchema = new mongoose.Schema({
   text: String,
   timestamp: { type: Number, default: Date.now }, // Use current timestamp by default
+  name: {type: String, 
+    default: function() {
+      return waifuNames[waifuNumber]; // Use a function for dynamic default
+    }},
+  color: {type: String, 
+    default: function() {
+      return neonColors[colorNumber]; // Use a function for dynamic default
+    }}
 });
 
 // Define the Message model
