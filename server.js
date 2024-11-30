668f6e9e5211f695f239ec7bdd65cd75b446cf6c
michaelzoub <micacao15@gmail.com>
2024-11-29 21:49:29 -0500

    added viewer count

--- server.js
+++ server.js
@@ -21,101 +21,100 @@ const server = http.createServer(app);
 ];
 
  const neonColors = [
-  "#FF6EC7", // Neon Pink
-  "#39FF14", // Neon Green
-  "#FFDF00", // Neon Yellow
-  "#FF073A", // Neon Red
-  "#00FFFF", // Neon Cyan
-  "#FF9933", // Neon Orange
-  "#9400D3", // Neon Purple
-  "#7DF9FF", // Neon Sky Blue
-  "#FBEC5D", // Neon Gold
-  "#FE59C2"  // Neon Magenta
+  "#FF6EC7", 
+  "#39FF14",
+  "#FFDF00", 
+  "#FF073A", 
+  "#00FFFF", 
+  "#FF9933",
+  "#9400D3", 
+  "#7DF9FF", 
+  "#FBEC5D", 
+  "#FE59C2"  
 ];
 
 
 const corsOptions = {
-  origin: ['https://waifuai-olive.vercel.app', "http://localhost:3000", "https://www.asuna.chat"],  // Replace with your Vercel app URL
+  origin: ['https://waifuai-olive.vercel.app', "http://localhost:3000", "https://www.asuna.chat"],  
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
-  credentials: true, // Allow cookies if needed
-};
+  credentials: true, 
+}
 
 app.use(cors(corsOptions));
 
-// Configure CORS to allow requests from localhost:3000
 const io = new Server(server, {
   cors: {
-    origin: ["https://waifuai-olive.vercel.app", "http://localhost:3000", "https://www.asuna.chat"],  // Allow only requests from this origin
-    methods: ["GET", "POST"],        // Specify allowed HTTP methods
-    allowedHeaders: ["Content-Type"], // Allowed headers
+    origin: ["https://waifuai-olive.vercel.app", "http://localhost:3000", "https://www.asuna.chat"], 
+    methods: ["GET", "POST"],        
+    allowedHeaders: ["Content-Type"], 
   }
 });
 
 const mongoURI = process.env.MONGO_URI
 
-// Connect to MongoDB
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB')
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+    console.error('Error connecting to MongoDB:', err)
   });
 
-//random number between 0-50
 const waifuNumber = Math.floor(Math.random() * 50)
 const colorNumber = Math.floor(Math.random() * 10)
 
-// Define the Message schema
 const messageSchema = new mongoose.Schema({
   text: String,
-  timestamp: { type: Number, default: Date.now }, // Use current timestamp by default
+  timestamp: { type: Number, default: Date.now }, 
   name: {type: String, 
     default: function() {
-      return waifuNames[waifuNumber]; // Use a function for dynamic default
+      return waifuNames[waifuNumber]; 
     }},
   color: {type: String, 
     default: function() {
-      return neonColors[colorNumber]; // Use a function for dynamic default
+      return neonColors[colorNumber]
     }}
-});
+})
 
 // Define the Message model
-const Message = mongoose.model("memecoin.delete after sunday", messageSchema);
+const Message = mongoose.model("memecoin.delete after sunday", messageSchema)
+
+let viewerCount = 0
 
 io.on("connection", (socket) => {
 
-  // Save incoming messages
+  viewerCount++
+
+  io.emit("viewerCount", viewerCount)
+
   socket.on("message", async (msg) => {
     try {
-      await Message.create(msg);  // Save message to MongoDB
-      io.emit("message", msg);     // Broadcast message to all clients
+      await Message.create(msg);  
+      io.emit("message", msg);     
     } catch (err) {
       console.error("Error saving message:", err);
     }
   });
 
-  // Load previous messages on connection
   socket.on("loadMessages", async () => {
     try {
-      const messages = await Message.find(); // Retrieve all previous messages from MongoDB
-      socket.emit("loadMessages", messages); // Send the messages back to the client
+      const messages = await Message.find(); 
+      socket.emit("loadMessages", messages); 
     } catch (err) {
       console.error("Error loading messages:", err);
     }
   });
 
-  // Handle client disconnect
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected")
+    viewerCount--
   });
 });
 
-// Start the server on port 3001
 server.listen(3001, () => {
   console.log("Server running on http://localhost:3001");
 });
