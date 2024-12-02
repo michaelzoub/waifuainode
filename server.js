112603bdecf4c6a9e9a40aedbdfeb0a23f927e11
michaelzoub <micacao15@gmail.com>
2024-12-02 14:41:16 -0500

    added broadcasting

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ io.on("connection", (socket) => {
 
   socket.on("asuna", async (asuna) => {
     try {
-      io.emit("asuna", asuna)
+      socket.broadcast.emit("asuna", asuna)
     } catch(error) {
       console.error("Can't chat with live audio: ", error)
     }
